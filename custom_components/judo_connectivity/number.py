b11ba4b9bd2f61d51c08affe9bde2dfb97cb26ba
marten-lucas <35145595+marten-lucas@users.noreply.github.com>
2025-02-21 19:23:10 +0100

    Update number.py
    AddEntitiesCallback

--- custom_components/judo_connectivity/number.py
+++ custom_components/judo_connectivity/number.py
@@ -5,7 +5,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfMass
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
 from .coordinator import JudoDataUpdateCoordinator
@@ -14,7 +14,7 @@ from .coordinator import JudoDataUpdateCoordinator
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
-    async_add_entities: AddConfigEntryEntitiesCallback,
+    async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the number platform."""
     coordinator: JudoDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
